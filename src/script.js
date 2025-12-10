a091aecf1aa1f47601c41b6d905c486eab45eab3
s4iOFFL <saisabarish.s.2023.cse@ritchennai.edu.in>
2025-12-10 18:37:42 +0530

    hhh

---
+++ src/script.js
@@ -0,0 +1,10 @@
+function autoType() {
+    var typed = new Typed(".auto-type", {
+        strings: ["FreeLancer", "Designer", "Gamer", "DsAist"],
+        typeSpeed: 150,
+        backSpeed: 150,
+        loop: true
+    });
+}
+
+autoType();
